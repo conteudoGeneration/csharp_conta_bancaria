097242130f896156c9879ecc53f9b5ae52e2c456
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-06-02 00:10:00 -0300

    Update Menu

--- Program.cs
+++ Program.cs
@@ -115,7 +115,9 @@ namespace conta_bancaria
                         keyPress();
                         break;
                     default:
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("\nOpção Inválida!\n");
+                        Console.ResetColor();
                         keyPress();
                         break;
                 }
