fc4d50f92ff1e01934fd2f5b30d3d9992a462a0b
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-06-23 03:59:21 -0300

    Updates

---
+++ repository/IContaRepository.cs
@@ -0,0 +1,19 @@
+﻿using conta_bancaria.model;
+
+namespace conta_bancaria.repository
+{
+    internal interface IContaRepository
+    {
+        // CRUD da Conta
+        public void ProcurarPorNumero(int numero);
+        public void ListarTodas();
+        public void Cadastrar(Conta conta);
+        public void Atualizar(Conta conta);
+        public void Deletar(int numero);
+
+        // Métodos Bancários
+        public void Sacar(int numero, float valor);
+        public void Depositar(int numero, float valor);
+        public void Transferir(int numeroOrigem, int numeroDestino, float valor);
+    }
+}
